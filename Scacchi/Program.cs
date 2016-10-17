54894461487e0eedf5afd087c272d0aad8eac560
Alberto Giacalone <alberto.giacalone@hotmail.com>
2016-10-17 23:21:22 +0200

    Version 1.1
    Added Scacchi.send(string a)

--- Scacchi/Program.cs
+++ Scacchi/Program.cs
@@ -2,31 +2,27 @@
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Xml.Serialization;
 using System.Collections.Generic;
+using System.Linq;
 
 using Emgu.CV;
-using Emgu.CV.Util;
 using Emgu.CV.Structure;
-using Emgu.Util;
-using System.Globalization;
-using System.Linq.Expressions;
+using Emgu.CV.CvEnum;
+using System.Diagnostics;
 using System.Threading;
-using System.Linq;
-using System.Reflection;
-
+using MiscUtil.IO;
 
 namespace Scacchi
 {
 	class MainClass
 	{
+		public static Bitmap bmp1 = new Bitmap (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img1.jpg"));
+		public static Bitmap bmp2 = new Bitmap (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img2.jpg"));
+		public static bool isAlreadyRunning = false;
+
 		public static void Main (string [] args)
 		{
-
-			string extension = "jpg";
-
-			Bitmap bmp1 = new Bitmap (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img1.jpg"));
-			Bitmap bmp2 = new Bitmap (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img2.jpg"));
+			/*
 			Bitmap bmp3 = getDifferencBitmap (bmp2, bmp1, Color.Red);
 
 			//bmp1.Save (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img1-mod.png"), ImageFormat.Png);
@@ -34,13 +30,14 @@ namespace Scacchi
 
 			bmp3.Save (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "img3.png"), ImageFormat.Png);
 
+
 			bmp1.Dispose ();
 			bmp2.Dispose ();
 			bmp3.Dispose ();
+			*/
 
-			//Detect_object();
-
-			//getResult ();
+			send ("e2e4");
+			send ("e2e4 e7e5");
 
 			Console.WriteLine ("Processo terminato");
 
@@ -57,15 +54,6 @@ namespace Scacchi
 
 			for (int y = 0; y < s1.Height; y++) {
 				for (int x = 0; x < s1.Width; x++) {
-
-					/*Color c1 = bmp1.GetPixel (x, y);
-					Color c2 = bmp2.GetPixel (x, y);
-
-					if (c1 == c2)
-						//bmp3.SetPixel (x, y, c1);
-						Thread.Sleep (0);
-					else
-						bmp3.SetPixel (x, y, diffColor);*/
 
 					//Un pò di colori...
 					byte black = 100;
@@ -152,87 +140,49 @@ namespace Scacchi
 			return bmp3;
 		}
 
-		/*public static void getResult ()
+		public static void send (string a)
 		{
 
-			const int width = 7; //width of chessboard no. squares in width - 1
-			const int height = 7; // heght of chess board no. squares in heigth - 1
-			Size patternSize = new Size (width, height); //size of chess board to be detected
+			// /Volumes/Macintosh SSD/Users/AlbertoGiacalone/Desktop/stockfish-7-mac/Mac/stockfish-7-64
+			string path = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Desktop), "output.txt");
 
-			Bgr [] line_colour_array = new Bgr [width * height]; // just for displaying coloured lines of detected chessboard
-			Image<Gray, Byte> [] Frame_array_buffer = new Image<Gray, byte> [100];
-			MCvPoint3D32f [] [] corners_object_list = new MCvPoint3D32f [Frame_array_buffer.Length] [];
-			PointF [] [] corners_points_list = new PointF [Frame_array_buffer.Length] [];
+			ProcessStartInfo processStartInfo = new ProcessStartInfo ("/Volumes/Macintosh SSD/Users/AlbertoGiacalone/Desktop/stockfish-7-mac/Mac/stockfish-7-64");
+			processStartInfo.RedirectStandardInput = true;
+			processStartInfo.RedirectStandardOutput = true;
+			processStartInfo.UseShellExecute = false;
 
-			IntrinsicCameraParameters IC = new IntrinsicCameraParameters ();
-			ExtrinsicCameraParameters [] EX_Param;
+			Process process = Process.Start (processStartInfo);
 
-			//Image<Gray, Byte> InputImage = new Image<Gray, byte> ("img1.png");
+			if (process != null) {
 
-			//CvInvoke.cvShowImage("image input: ", InputImage.Ptr);
-
-			//PointF [] corners = new PointF [] { };
-			//corners = CameraCalibration.FindChessboardCorners (InputImage, PatternSize, Emgu.CV.CvEnum.CALIB_CB_TYPE.ADAPTIVE_THRESH | Emgu.CV.CvEnum.CALIB_CB_TYPE.FILTER_QUADS);
-
-			CameraCalibration.DrawChessboardCorners (Frame_array_buffer, patternSize, corners_points_list);
-
-			//CvInvoke.cvShowImage("result: ", InputImage.Ptr);
-
-		}*/
-
-		/*public void matchimage(System.Drawing.Bitmap img1, System.Drawing.Bitmap img2) {
-
-			string img1_ref, img2_ref;
-			int count1 = 0, count2 = 0;
-			bool flag = true;
-
-			if (img1.Width == img2.Width && img1.Height == img2.Height) {
-				for (int i = 0; i < img1.Width; i++) {
-					for (int j = 0; j < img1.Height; j++) {
-						img1_ref = img1.GetPixel(i, j).ToString();
-						img2_ref = img2.GetPixel(i, j).ToString();
-						if (img1_ref != img2_ref) {
-							count2++;
-							flag = false;
-							break;
-						}
-						count1++;
-					}
-
+				if (!isAlreadyRunning) {
+					process.StandardInput.WriteLine ("uci");
+					process.StandardInput.WriteLine ("isready");
+					process.StandardInput.WriteLine ("ucinewgame");
+					isAlreadyRunning = true;
 				}
-				if (flag == false)
-					MessageBox.Show("Sorry, Images are not same , " + count2 + " wrong pixels found");
-				else
-					MessageBox.Show(" Images are same , " + count1 + " same pixels found and " + count2 + " wrong pixels found");
-			} else
-				MessageBox.Show("can not compare this images");
 
-			img1.Dispose();
-			img2.Dispose();
-		}*/
+				process.StandardInput.WriteLine ("position startpos moves " + a);
+				process.StandardInput.WriteLine ("go movetime 2000");
+				Thread.Sleep (2010);
+				//Console.WriteLine (process.StandardOutput.ReadToEnd ());
 
-		/*public static void Detect_object() {
+				process.StandardInput.Close (); // line added to stop process from hanging on ReadToEnd()
 
-			Size patternSize = new Size(8, 8);
+				string outputString = process.StandardOutput.ReadToEnd ();
+				string lastline = "";
+				for (int i = 1; i < outputString.Split ('\n').Length; i++) {
+					if (i == (outputString.Split ('\n').Length - 2)) {
+						lastline = outputString.Split ('\n') [i];
+					}
+				}
 
-			Image<Gray, Byte> chessboardImage = new Image<Gray, byte>(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "img1-old.png"));
-			//PointF[] corners;
-			PointF[] corners = 
-				CameraCalibration.FindChessboardCorners(
-					chessboardImage,
-					patternSize,
-					Emgu.CV.CvEnum.CALIB_CB_TYPE.ADAPTIVE_THRESH | Emgu.CV.CvEnum.CALIB_CB_TYPE.NORMALIZE_IMAGE | Emgu.CV.CvEnum.CALIB_CB_TYPE.FILTER_QUADS
-				);
+				Console.WriteLine (lastline);
+			}
 
-			chessboardImage.FindCornerSubPix(
-				new PointF[][] { corners }, 
-				new Size(10, 10),
-				new Size(-1, -1),
-				new MCvTermCriteria(0.05));
 
-			CameraCalibration.DrawChessboardCorners(chessboardImage, patternSize, corners);
+		}
 
-		}*/
 
 	}
 }
